f4ecdf73b1a0b79053cc435aba6503206e44d59b
Forrest Wilson <forrest.wilson@me.com>
2017-11-13 21:27:28 +1300

    Added a .round() and .getTotal() method to the GST Object

---
+++ gulpfile.js
@@ -0,0 +1,3 @@
+var gulp = require("gulp"),
+    clean = require("gulp-clean"),
+    uglify = require("gulp-uglify");
--- src/gst.js
+++ src/gst.js
@@ -1,3 +1,17 @@
-(function() {
+var GST = {};
 
-})();
+GST.gstPercent = 0.15;
+
+GST.round = function(value, decimals) {
+    return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
+}
+
+GST.getTotal = function(subtotal) {
+    var sub = parseFloat(subtotal);
+    var roundedSub = GST.round(sub, 2);
+
+    var total = roundedSub * (1 + GST.gstPercent);
+    var roundedTotal = GST.round(total, 2);
+
+    return roundedTotal;
+}
