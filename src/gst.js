93863ad3a81967e011c186f1d474faeaeda64d42
Forrest Wilson <forrest.wilson@me.com>
2017-11-14 15:14:43 +1300

    Commenting

--- src/gst.js
+++ src/gst.js
@@ -2,12 +2,15 @@
 
 var GST = {};
 
+// Default GST Percentage of 15%
 GST.gstPercent = 0.15;
 
+// Round the number to 2dp
 GST.round = function(value, decimals) {
     return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
 }
 
+// Get the total by passing in the subtotal and percentage
 GST.getTotal = function(subtotal) {
     var sub = parseFloat(subtotal);
     var roundedSub = GST.round(sub, 2);
@@ -18,6 +21,7 @@ GST.getTotal = function(subtotal) {
     return roundedTotal;
 }
 
+// Get the total by passing in the total and percentage
 GST.getSubtotal = function(total) {
     var tot = parseFloat(total);
     var roundedTot = GST.round(tot, 2);
