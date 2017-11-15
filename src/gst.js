1a89e9a916b304e54ab4612676e27f5d689df2f4
Forrest Wilson <forrest.wilson@me.com>
2017-11-15 14:16:26 +1300

    Conversion to literal Object declarations

--- src/gst.js
+++ src/gst.js
@@ -1,40 +1,41 @@
-"use strict";
+var GST = (function() {
 
-var GST = {};
+    "use strict";
 
-// Default GST Percentage of 15%
-GST.gstPercent = 0.15;
+    var GST = {
+        percent: 0.15,
+        denom: function() {
+            return this.percent + 1;
+        },
+        round: function(val, dec) {
+            if (typeof dec === "undefined" || typeof dec === "string") dec = 2;
+            
+            return Number(Math.round(val + "e" + dec) + "e-" + dec);
+        },
+        getTotal: function(subtotal) {
+            // Parses the string/number passed in to the function
+            var sub = parseFloat(subtotal),
+                roundSub = this.round(sub),
+                total = roundSub * this.denom(),
+                roundTotal = this.round(total);
+    
+            return roundTotal;
+        },
+        getSubtotal: function(total) {
+            // Parses the string/number passed in to the function
+            var tot = parseFloat(total),
+                roundTot = this.round(tot),
+                subtotal = roundTot / this.denom(),
+                roundSub = this.round();
+    
+            return roundSub;
+        },
+        getGST: function(total, sub) {
+            var roundGST = GST.round(total - sub);
+            return roundGST;
+        }
+    };
 
-// Round the number to 2dp
-GST.round = function(value, decimals) {
-    if (typeof decimals === "undefined" || typeof decimals === "string") decimals = 2;
+    return GST;
 
-    return Number(Math.round(value + "e" + decimals) + "e-" + decimals);
-}
-
-// Get the total by passing in the subtotal and percentage
-GST.getTotal = function(subtotal) {
-    // Parses the string/number passed in to the function
-    var sub = parseFloat(subtotal),
-        roundedSub = GST.round(sub),
-        total = roundedSub * (1 + GST.gstPercent),
-        roundedTotal = GST.round(total);
-
-    return roundedTotal;
-}
-
-// Get the total by passing in the total and percentage
-GST.getSubtotal = function(total) {
-    // Parses the string/number passed in to the function
-    var tot = parseFloat(total),
-        roundedTot = GST.round(tot),
-        subtotal = roundedTot / (1 + GST.gstPercent),
-        roundedSubtotal = GST.round(subtotal);
-
-    return roundedSubtotal;
-}
-
-// Get the GST percentage by passing in the total and subtotal
-GST.getGST = function(total, subtotal) {
-    return total - subtotal;
-}
+})();
