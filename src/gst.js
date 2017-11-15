008e7a9c8a40c8600412aa6d733a241aa7a78744
Forrest Wilson <forrest.wilson@me.com>
2017-11-15 14:38:55 +1300

    simplified and removed the .round() method in favour of the built in toFixed() method

--- src/gst.js
+++ src/gst.js
@@ -5,31 +5,24 @@ var GST = (function() {
         denom: function() {
             return this.percent + 1;
         },
-        round: function(val, dec) {
-            if (typeof dec === "undefined" || typeof dec === "string") dec = 2;
-            
-            return Number(Math.round(val + "e" + dec) + "e-" + dec);
-        },
         getTotal: function(subtot) {
-            // Parses the string/number passed in to the function
             var sub = parseFloat(subtot),
-                roundSub = this.round(sub),
+                roundSub = sub.toFixed(2),
                 tot = roundSub * this.denom(),
-                roundTotal = this.round(tot);
+                roundTotal = tot.toFixed(2);
     
             return roundTotal;
         },
         getSubtotal: function(total) {
-            // Parses the string/number passed in to the function
             var tot = parseFloat(total),
-                roundTot = this.round(tot),
+                roundTot = tot.toFixed(2),
                 subtotal = roundTot / this.denom(),
-                roundSub = this.round();
+                roundSub = subtotal.toFixed(2);
     
             return roundSub;
         },
-        getGST: function(total, sub) {
-            var roundGST = this.round(total - sub);
+        getGST: function(tot, sub) {
+            var roundGST = (tot - sub).toFixed(2);
             return roundGST;
         }
     };
