474540753490414324af77560b6b371fdc8bdf71
Forrest Wilson <forrest.wilson@me.com>
2018-02-07 08:38:59 +1300

    Small workflow settings changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,12 @@ var gulp = require("gulp"),
     runSequence = require("run-sequence"),
     rename = require("gulp-rename");
 
-// Minifies the JS found in the temp/js folder
 gulp.task("minify-js", () => {
     return gulp.src("./gstjs/gst.js")
         .pipe(jshint())
         .pipe(jshint.reporter("default"))
         .pipe(uglify())
-        .pipe(rename("gst.min.js"))
+        .pipe(rename({suffix: ".min"}))
         .pipe(gulp.dest("./gstjs/"));
 });
 
